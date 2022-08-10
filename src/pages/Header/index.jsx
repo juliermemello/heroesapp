842524deb447c9750c05e51cecb48cd4ec6eb884
juliermemello@gmail.com <juliermemello@gmail.com>
2022-08-10 18:30:20 -0300

    Refactoring the search component

--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -1,49 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { AppBar, Box, InputBase, Toolbar, Typography } from "@mui/material";
-import { styled, alpha } from "@mui/material/styles";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+
 import { useFilter } from "hooks/useFilter";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(1),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "14ch",
-      "&:focus": {
-        width: "22ch",
-      },
-    },
-  },
-}));
+import {
+  Search,
+  SearchIconWrapper,
+  StyledInputBase,
+} from "./Components/Search";
 
 function Header() {
   const [filterValue, setFilterValue] = useState("");
@@ -57,25 +21,23 @@ function Header() {
   }, [filterValue]);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky" elevation={0}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Heroes App
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              value={filterValue}
-              onChange={(e) => setFilterValue(e.target.value)}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky" elevation={0}>
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Heroes App
+        </Typography>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            value={filterValue}
+            onChange={(e) => setFilterValue(e.target.value)}
+          />
+        </Search>
+      </Toolbar>
+    </AppBar>
   );
 }
 
